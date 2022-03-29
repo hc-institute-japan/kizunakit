fb8a99a3d923da686e0d7c513bea79c9be535a07
Ella Gopez <46973844+gopezella@users.noreply.github.com>
2022-03-30 00:06:45 +0800

    Add icons

--- kizuna-design-kit/src/components.d.ts
+++ kizuna-design-kit/src/components.d.ts
@@ -60,13 +60,6 @@ export namespace Components {
         "text": string;
         "type": string;
     }
-    interface KizunaModal {
-        "classes": { [key: string]: any };
-        "closeIcon": Element;
-        "onClose": Function;
-        "open": any;
-        "transparent": boolean;
-    }
     interface KizunaPopover {
         "buttonText": string;
         "darkmode": boolean;
@@ -148,12 +141,6 @@ declare global {
         prototype: HTMLKizunaListItemElement;
         new (): HTMLKizunaListItemElement;
     };
-    interface HTMLKizunaModalElement extends Components.KizunaModal, HTMLStencilElement {
-    }
-    var HTMLKizunaModalElement: {
-        prototype: HTMLKizunaModalElement;
-        new (): HTMLKizunaModalElement;
-    };
     interface HTMLKizunaPopoverElement extends Components.KizunaPopover, HTMLStencilElement {
     }
     var HTMLKizunaPopoverElement: {
@@ -187,7 +174,6 @@ declare global {
         "kizuna-checkbox": HTMLKizunaCheckboxElement;
         "kizuna-icon": HTMLKizunaIconElement;
         "kizuna-list-item": HTMLKizunaListItemElement;
-        "kizuna-modal": HTMLKizunaModalElement;
         "kizuna-popover": HTMLKizunaPopoverElement;
         "kizuna-tabs": HTMLKizunaTabsElement;
         "kizuna-textbox": HTMLKizunaTextboxElement;
@@ -250,13 +236,6 @@ declare namespace LocalJSX {
         "text"?: string;
         "type"?: string;
     }
-    interface KizunaModal {
-        "classes"?: { [key: string]: any };
-        "closeIcon"?: Element;
-        "onClose"?: Function;
-        "open"?: any;
-        "transparent"?: boolean;
-    }
     interface KizunaPopover {
         "buttonText"?: string;
         "darkmode"?: boolean;
@@ -297,7 +276,6 @@ declare namespace LocalJSX {
         "kizuna-checkbox": KizunaCheckbox;
         "kizuna-icon": KizunaIcon;
         "kizuna-list-item": KizunaListItem;
-        "kizuna-modal": KizunaModal;
         "kizuna-popover": KizunaPopover;
         "kizuna-tabs": KizunaTabs;
         "kizuna-textbox": KizunaTextbox;
@@ -316,7 +294,6 @@ declare module "@stencil/core" {
             "kizuna-checkbox": LocalJSX.KizunaCheckbox & JSXBase.HTMLAttributes<HTMLKizunaCheckboxElement>;
             "kizuna-icon": LocalJSX.KizunaIcon & JSXBase.HTMLAttributes<HTMLKizunaIconElement>;
             "kizuna-list-item": LocalJSX.KizunaListItem & JSXBase.HTMLAttributes<HTMLKizunaListItemElement>;
-            "kizuna-modal": LocalJSX.KizunaModal & JSXBase.HTMLAttributes<HTMLKizunaModalElement>;
             "kizuna-popover": LocalJSX.KizunaPopover & JSXBase.HTMLAttributes<HTMLKizunaPopoverElement>;
             "kizuna-tabs": LocalJSX.KizunaTabs & JSXBase.HTMLAttributes<HTMLKizunaTabsElement>;
             "kizuna-textbox": LocalJSX.KizunaTextbox & JSXBase.HTMLAttributes<HTMLKizunaTextboxElement>;
--- kizuna-design-kit/src/components/kizuna-icon/assets/index.ts
+++ kizuna-design-kit/src/components/kizuna-icon/assets/index.ts
@@ -1,17 +1,101 @@
-import search from './search.svg';
-import settings from './settings.svg';
-import emoticon from './emoticon.svg';
+import addContact from './add-participant.svg';
+import add from './add.svg';
+import attach from './attach.svg';
+import blocked from './blocked.svg';
+import bold from './bold.svg';
+import briefcase from './briefcase.svg';
 import calendar from './calendar.svg';
-import trash from './trash.svg';
+import call from './call.svg';
+import cameraOff from './camera-off.svg';
+import camera from './camera.svg';
+import caution from './caution.svg';
+import chat from './chat.svg';
+import check from './check.svg';
 import circle from './circle.svg';
+import close from './close.svg';
+import contacts from './contacts.svg';
+import copy from './copy.svg';
+import create from './create.svg';
+import dottedMenuHorizontal from './dotted-menu-horizontal.svg';
+import dottedMenuVertical from './dotted-menu-vertical.svg';
+import down from './down.svg';
+import end from './end.svg';
+import emoticon from './emoticon.svg';
+import emptyNotification from './empty-notification.svg';
+import file from './file.svg';
+import gif from './gif.svg';
+import information from './information.svg';
+import italic from './italic.svg';
+import leftArrow from './left-arrow.svg';
+import left from './left.svg';
+import link from './link.svg';
+import menu from './menu.svg';
+import message from './message.svg';
+import mic from './mic.svg';
+import micOff from './mic-off.svg';
+import pin from './pin.svg';
+import pin2 from './pin2.svg';
+import remove from './remove.svg';
+import reply from './reply.svg';
+import rightArrow from './right-arrow.svg';
+import right from './right.svg';
+import search from './search.svg';
+import send from './send.svg';
+import settings from './right.svg';
+import strikethrough from './strikethrough.svg';
+import trash from './trash.svg';
+import underline from './underline.svg';
+import up from './up.svg';
 
 const icons = {
-  search,
-  settings,
-  emoticon,
+  addContact,
+  add,
+  attach,
+  blocked,
+  bold,
+  briefcase,
   calendar,
-  trash,
+  call,
+  cameraOff,
+  camera,
+  caution,
+  chat,
+  check,
   circle,
+  close,
+  contacts,
+  copy,
+  create,
+  dottedMenuHorizontal,
+  dottedMenuVertical,
+  down,
+  emoticon,
+  emptyNotification,
+  end,
+  file,
+  gif,
+  information,
+  italic,
+  left,
+  leftArrow,
+  link,
+  menu,
+  message,
+  mic,
+  micOff,
+  pin,
+  pin2,
+  remove,
+  reply,
+  right,
+  rightArrow,
+  search,
+  send,
+  settings,
+  strikethrough,
+  trash,
+  underline,
+  up,
 };
 
 export function getIcon(name) {
