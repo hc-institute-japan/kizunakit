64ade326225edf3ce7c440d8577c1dd3d367c7df
Ella Gopez <gopezellam@gmail.com>
2022-04-01 03:53:27 +0800

    Add Table Component
    - use to display metadata of components

---
+++ react-ui/src/components/CustomTable/CustomTable.jsx
@@ -0,0 +1,47 @@
+import styles from './CustomTable.module.css';
+
+const CustomTable = (props: any) => {
+  const TableHeaders = {
+    API: ['Name', 'Type', 'Default Value', 'Description'],
+    CSS: ['Name', 'Description'],
+  };
+
+  const _renderTableHeader = () => {
+    return TableHeaders[props?.type || 'API'].map(header => {
+      return <th className={styles.tableHeader}>{header}</th>;
+    });
+  };
+
+  const _renderTabledata = () => {
+    if (props?.type === 'API') {
+      return props?.data?.map(data => {
+        return (
+          <tr className={styles.tableHeaders}>
+            <td className={styles.tableHeader}>{data.name}</td>
+            <td className={styles.tableHeader}>{data.type}</td>
+            <td className={styles.tableHeader}>{data.defaultValue}</td>
+            <td className={styles.tableHeader}>{data.description}</td>
+          </tr>
+        );
+      });
+    } else {
+      return props?.data?.map(data => {
+        return (
+          <tr className={styles.tableHeaders}>
+            <td className={styles.tableHeader}>{data.name}</td>
+            <td className={styles.tableHeader}>{data.description}</td>
+          </tr>
+        );
+      });
+    }
+  };
+
+  return (
+    <table className={styles.tableWrapper}>
+      <thead className={styles.tableHeaders}>{_renderTableHeader()}</thead>
+      {_renderTabledata()}
+    </table>
+  );
+};
+
+export default CustomTable;
--- react-ui/src/pages/Buttons/Buttons.tsx
+++ react-ui/src/pages/Buttons/Buttons.tsx
@@ -1,7 +1,29 @@
 import { KizunaButton } from 'kizuna-react-kit';
 import styles from './Buttons.module.css';
+import CustomTable from '../../components/CustomTable/CustomTable';
 
 const ButtonsPage = () => {
+  const API_DATA = [
+    {
+      name: 'name1',
+      type: 'string',
+      defaultValue: '',
+      description: 'setsetset',
+    },
+    { name: 'name2', type: 'string', defaultValue: '', description: 'testse' },
+    { name: 'name3', type: 'boolean', defaultValue: '', description: '' },
+    { name: 'name4', type: 'object', defaultValue: '', description: '' },
+  ];
+
+  const CSS_DATA = [
+    {
+      name: 'name1',
+      description: 'setsetset',
+    },
+    { name: 'name2', description: 'testse' },
+    { name: 'name3', type: 'boolean', description: '' },
+  ];
+
   return (
     <div>
       <h1>BUTTONS PAGE </h1>
@@ -28,6 +50,18 @@ const ButtonsPage = () => {
           }}
         />
       </div>
+
+      <hr />
+      <h1>API</h1>
+      <p>Use these Props to access features of the component</p>
+      <CustomTable type="API" data={API_DATA} />
+      <hr />
+
+      <h1>CSS</h1>
+      <p>Use these class names to override styling</p>
+      <CustomTable type="CSS" data={CSS_DATA} />
+
+      <h1>EXAMPLE</h1>
     </div>
   );
 };
