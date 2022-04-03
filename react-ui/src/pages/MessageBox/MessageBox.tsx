e5c3f97138dcdc03d52085890a3bb378e5656554
Ella Gopez <46973844+gopezella@users.noreply.github.com>
2022-04-04 00:43:00 +0800

    Add Message Box Display Page

--- react-ui/src/pages/MessageBox/MessageBox.tsx
+++ react-ui/src/pages/MessageBox/MessageBox.tsx
@@ -1,9 +1,244 @@
 import styles from './MessageBox.module.css';
+import CustomTabs from '../../components/Tabs/Tabs';
+import CustomTable from '../../components/CustomTable/CustomTable';
+
+import { KizunaMessageBox } from 'kizuna-react-kit';
+import { KizunaDivider } from 'kizuna-react-kit';
+import { useState } from 'react';
 
 const MessageBox = (props: any) => {
+  const [selected, setSelected] = useState('examples');
+
+  const API_DATA = [
+    {
+      name: 'message',
+      type: 'string',
+      defaultValue: '',
+      description: 'The displayed message of the message box.',
+    },
+    {
+      name: 'time',
+      type: 'string',
+      defaultValue: '',
+      description: 'Displayed time of the message box',
+    },
+    {
+      name: 'messageStatus',
+      type: "'sent' | 'delivered' | 'seen' ",
+      defaultValue: '',
+      description: 'Sets the message status of the message',
+    },
+    {
+      name: 'darkmode',
+      type: 'boolean',
+      defaultValue: '',
+      description: '',
+    },
+    {
+      name: 'sent',
+      type: 'boolean',
+      defaultValue: '',
+      description: 'Sets the message box if user or contact',
+    },
+    {
+      name: 'classes',
+      type: 'object',
+      defaultValue: '',
+      description:
+        'An object of custom class names of the component. Please refer to CSS tab for more information.',
+    },
+  ];
+
+  const CSS_DATA = [
+    {
+      name: '',
+      description: '',
+    },
+  ];
   return (
     <>
-      <h1>MessageBox</h1>
+      <div className={styles.messageBoxPage}>
+        <h1 className={styles.title}>Message Box</h1>
+        <CustomTabs value={selected} onClickHandler={setSelected} />
+        {selected === 'examples' && (
+          <div>
+            <div className={styles.messageBoxExamplesContainer}>
+              <div className={styles.default}>
+                <div className={styles.userMessageBox}>
+                  <h3>User</h3>
+                  <div className={styles.default}>
+                    <div>
+                      <h4>Sent</h4>
+                      <KizunaMessageBox
+                        message="Welcome to Kizuna!"
+                        time="10:00 AM"
+                        messageStatus="sent"
+                      />
+                    </div>
+
+                    <div>
+                      <h4>Delivered</h4>
+                      <KizunaMessageBox
+                        message="Welcome to Kizuna!"
+                        time="10:00 AM"
+                        messageStatus="delivered"
+                      />
+                    </div>
+
+                    <div>
+                      <h4>Seen</h4>
+                      <KizunaMessageBox
+                        message="Welcome to Kizuna!"
+                        time="10:00 AM"
+                        messageStatus="seen"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.contactMessageBox}>
+                  <h3>Contact</h3>
+                  <div className={styles.contactMessageBoxContainer}>
+                    <div>
+                      <h4>Sent</h4>
+                      <KizunaMessageBox
+                        message="Welcome to Kizuna!"
+                        time="10:00 AM"
+                        sent
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <br />
+
+              <div className={styles.darkModeWrapper}>
+                <h2>Dark Mode</h2>
+                <div className={styles.darkMode}>
+                  <div className={styles.userMessageBox}>
+                    <h3>User</h3>
+                    <div className={styles.default}>
+                      <div>
+                        <h4>Sent</h4>
+                        <KizunaMessageBox
+                          message="Welcome to Kizuna!"
+                          time="10:00 AM"
+                          messageStatus="sent"
+                          darkmode
+                        />
+                      </div>
+
+                      <div>
+                        <h4>Delivered</h4>
+                        <KizunaMessageBox
+                          message="Welcome to Kizuna!"
+                          time="10:00 AM"
+                          messageStatus="delivered"
+                          darkmode
+                        />
+                      </div>
+
+                      <div>
+                        <h4>Seen</h4>
+                        <KizunaMessageBox
+                          message="Welcome to Kizuna!"
+                          time="10:00 AM"
+                          messageStatus="seen"
+                          darkmode
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.contactMessageBox}>
+                    <h3>Contact</h3>
+                    <div className={styles.contactMessageBoxContainer}>
+                      <div>
+                        <h4>Sent</h4>
+                        <KizunaMessageBox
+                          message="Welcome to Kizuna!"
+                          time="10:00 AM"
+                          sent
+                          darkmode
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <KizunaDivider classes={{ divider: styles.dividerMargin }} />
+            <h1 className={styles.title}>Examples</h1>
+
+            <div className={styles.codeExampleContainer}>
+              <small>Default</small>
+
+              <code>
+                {
+                  '<KizunaMessageBox message="Welcome to Kizuna!" time="10:00 AM" messageStatus="sent" />'
+                }
+              </code>
+              <code>
+                {
+                  '<KizunaMessageBox message="Welcome to Kizuna!" time="10:00 AM" messageStatus="delivered" />'
+                }
+              </code>
+              <code>
+                {
+                  '<KizunaMessageBox message="Welcome to Kizuna!" time="10:00 AM" messageStatus="seen" />'
+                }
+              </code>
+              <code>
+                {
+                  '<KizunaMessageBox message="Welcome to Kizuna!" time="10:00 AM" sent />'
+                }
+              </code>
+              <small>Dark Mode</small>
+              <code>
+                {
+                  '<KizunaMessageBox message="Welcome to Kizuna!" time="10:00 AM" messageStatus="sent" darkmode />'
+                }
+              </code>
+              <code>
+                {
+                  '<KizunaMessageBox message="Welcome to Kizuna!" time="10:00 AM" messageStatus="delivered" darkmode />'
+                }
+              </code>
+              <code>
+                {
+                  '<KizunaMessageBox message="Welcome to Kizuna!" time="10:00 AM" messageStatus="seen" darkmode />'
+                }
+              </code>
+
+              <code>
+                {
+                  '<KizunaMessageBox message="Welcome to Kizuna!" time="10:00 AM" sent darkmode />'
+                }
+              </code>
+            </div>
+          </div>
+        )}
+        {selected === 'api' && (
+          <div className={styles.apiContainer}>
+            <h1 className={styles.title}>API</h1>
+            <p>Use these Props to access features of the component</p>
+            <CustomTable type="API" data={API_DATA} />
+          </div>
+        )}
+        {selected === 'css' && (
+          <div>
+            <h1 className={styles.title}>CSS</h1>
+            <p>Use these class names to override styling</p>
+            <CustomTable type="CSS" data={CSS_DATA} />
+            <KizunaDivider classes={{ divider: styles.dividerMargin }} />
+            <h1 className={styles.title}>Example</h1>
+
+            <div className={styles.codeExampleContainer}>
+              <code>{'insert code here'}</code>
+              <code>{'insert code here'}</code>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
