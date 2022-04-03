5663a34f444cee84557cb6b1740f574b09eb5876
Ella Gopez <gopezellam@gmail.com>
2022-04-04 02:44:43 +0800

    Add Toggle Display Page

--- react-ui/src/pages/Toggle/Toggle.tsx
+++ react-ui/src/pages/Toggle/Toggle.tsx
@@ -1,9 +1,139 @@
 import styles from './Toggle.module.css';
+import CustomTabs from '../../components/Tabs/Tabs';
+import CustomTable from '../../components/CustomTable/CustomTable';
+
+import { KizunaToggleButton } from 'kizuna-react-kit';
+import { KizunaDivider } from 'kizuna-react-kit';
+import { useState } from 'react';
 
 const Toggle = (props: any) => {
+  const [selected, setSelected] = useState('examples');
+
+  const API_DATA = [
+    {
+      name: 'checked',
+      type: 'boolean',
+      defaultValue: '',
+      description: 'Value if button is active or not',
+    },
+    {
+      name: 'disabled',
+      type: 'booblean',
+      defaultValue: 'dalse',
+      description: 'disables the component',
+    },
+    {
+      name: 'classes',
+      type: 'object',
+      defaultValue: '',
+      description:
+        'An object of custom class names of the component. Please refer to CSS tab for more information.',
+    },
+  ];
+
+  const CSS_DATA = [
+    {
+      name: 'badgeWrapper',
+      description: 'Overides the styling for the main wrapper of the badge',
+    },
+    { name: 'icon', description: 'Overides the styling for the icon wrapper' },
+  ];
   return (
     <>
-      <h1>Toggle</h1>
+      <div className={styles.badgePage}>
+        <h1 className={styles.title}>Toggle</h1>
+        <CustomTabs value={selected} onClickHandler={setSelected} />
+        {selected === 'examples' && (
+          <div>
+            <div className={styles.badgeExamplesContainer}>
+              <div className={styles.default}>
+                <h3>Default</h3>
+                <KizunaToggleButton />
+              </div>
+
+              <div className={styles.default}>
+                <h3>Disabled</h3>
+                <KizunaToggleButton disabled />
+              </div>
+
+              <div className={styles.customStyles}>
+                <h3>Custom Styles</h3>
+                <KizunaToggleButton
+                  classes={{
+                    slider: styles.customSlider,
+                    input: styles.customInput,
+                  }}
+                />
+              </div>
+            </div>
+            <KizunaDivider classes={{ divider: styles.dividerMargin }} />
+            <h1 className={styles.title}>Examples</h1>
+
+            <div className={styles.codeExampleContainer}>
+              <small>Default</small>
+
+              <code>{'<KizunaToggleButton />'}</code>
+              <small>Disabled</small>
+              <code>{' <KizunaToggleButton disabled />'}</code>
+              <small>Custom Styles</small>
+              <code>
+                {`                <KizunaToggleButton
+                  classes={{
+                    slider: styles.customSlider,
+                    input: styles.customInput,
+                  }}
+                />`}
+              </code>
+            </div>
+          </div>
+        )}
+        {selected === 'api' && (
+          <div className={styles.apiContainer}>
+            <h1 className={styles.title}>API</h1>
+            <p>Use these Props to access features of the component</p>
+            <CustomTable type="API" data={API_DATA} />
+          </div>
+        )}
+        {selected === 'css' && (
+          <div>
+            <h1 className={styles.title}>CSS</h1>
+            <p>Use these class names to override styling</p>
+            <CustomTable type="CSS" data={CSS_DATA} />
+            <KizunaDivider classes={{ divider: styles.dividerMargin }} />
+            <h1 className={styles.title}>Example</h1>
+
+            <div className={styles.codeExampleContainer}>
+              <code>
+                {`                <KizunaToggleButton
+                  classes={{
+                    slider: styles.customSlider,
+                    input: styles.customInput,
+                  }}
+                />`}
+              </code>
+            </div>
+
+            <h4>Styles</h4>
+            <div className={styles.codeExampleContainer}>
+              <code>
+                {`.customSlider {
+  background-color: #dde8c5;
+}`}
+              </code>
+              <code>
+                {`.customInput:checked + .customSlider {
+  background: #b2ff12;
+}`}
+              </code>
+              <code>
+                {`.customInput:checked + .customSlider:before {
+  background: #16ad00;
+}`}
+              </code>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
