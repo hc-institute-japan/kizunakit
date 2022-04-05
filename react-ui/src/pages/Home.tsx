8b9211470fbfbb74181f7156efb982d8d8f00608
Ella Gopez <gopezellam@gmail.com>
2022-04-05 23:18:25 +0800

    Fix Code Display

--- react-ui/src/pages/Home.tsx
+++ react-ui/src/pages/Home.tsx
@@ -18,7 +18,34 @@ const Home = () => {
       </div>
 
       <h2>Browser Global</h2>
-      <div className={styles.codeWrapper}></div>
+      <div className={styles.codeWrapper}>
+        <p>{':root {'}</p>
+        <p>{'   --primary: #e48221;'}</p>
+        <p>{'   --primary-10: #e5c8ab;'}</p>
+        <p>
+          {
+            '--primary-gradient: linear-gradient(180deg, #e47221 0%, #f5c31a 100%);'
+          }
+        </p>
+        <br />
+        <p>{'/* Light Mode */'}</p>
+        <p>{'--primary-text: #404040;'}</p>
+        <p>{'--secondary-text: #838383;'}</p>
+        <br />
+        <p>{'/* Dark Mode */'}</p>
+        <p>{'-dark-primary-text: #cfcfcf;'}</p>-
+        <p>{'--dark-secondary-text: #979797;'}</p>
+        <p>{'--dark-primary-bg: #23252e;'}</p>
+        <p>{'--dark-secondary-bg: #292b33;'}</p>
+        <br />
+        <p>{'/* Colors */'}</p>
+        <p>{'-white: #ffff;'}</p>
+        <p>{'--black: #0000;'}</p>
+        <p>{'--red: #f10404;'}</p>
+        <p>{'--green: #11d30d;'}</p>
+        <p>{'--gray: #f3f3f3;'}</p>
+        <p>{'}'}</p>
+      </div>
     </div>
   );
 };
