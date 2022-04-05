79edfb7ae69215293519fee799d47b5388ab5f7f
Ella Gopez <gopezellam@gmail.com>
2022-04-05 23:13:37 +0800

    Add Home Content

--- react-ui/src/pages/Home.tsx
+++ react-ui/src/pages/Home.tsx
@@ -1,7 +1,24 @@
+import styles from './Home.module.css';
+
 const Home = () => {
   return (
-    <div className="">
-      <h1>Installation</h1>
+    <div className={styles.homePage}>
+      <h1>Introduction</h1>
+      <p>Learn how to include Kizuna Kit in your project</p>
+
+      <h2>Installation</h2>
+
+      <div className={styles.codeWrapper}>
+        <code>{'npm install kizuna-react-kit'}</code>
+      </div>
+
+      <h2>Importing Components</h2>
+      <div className={styles.codeWrapper}>
+        <code>{"import { KizunaButton } from 'kizuna-react-kit' "}</code>
+      </div>
+
+      <h2>Browser Global</h2>
+      <div className={styles.codeWrapper}></div>
     </div>
   );
 };
